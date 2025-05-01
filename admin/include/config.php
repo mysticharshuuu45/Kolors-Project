b81738ddd682bf6182f8a16d2b30942b36e0eb78
mysticharshuuu45 <126454260+mysticharshuuu45@users.noreply.github.com>
2025-05-01 12:13:33 +0530

    Update config.php

--- admin/include/config.php
+++ admin/include/config.php
@@ -1,6 +1,7 @@
+
 <?php
     define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_USER','kolors');
+    define('DB_PASS','harsh@4545');
     define('DB_NAME','dbwedding');
 ?>
